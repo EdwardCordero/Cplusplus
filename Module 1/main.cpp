34e9af3ec8c0132f10481ffe334fed62d08524de
Edward Cordero <edwardcordero98@gmail.com>
2025-10-30 19:34:12 -0700

    Created function to generate char*[] and allow it to be mutable by converting string literal into chars and allocating char* and char*[] in heap

--- Module 1/main.cpp
+++ Module 1/main.cpp
@@ -28,18 +28,68 @@ void stringCopy(char* destination, const char* src)
     destination[j++] = '\0';
 }
 
+void reverseStr(char* str)
+{
+    int end = stringLen(str) - 1;
+    int start = 0;
+    char c = str[start];
+    
+    while(start <= end)
+    {
+        char sc = str[start];
+        char ec = str[end];
+        char temp = sc;
+
+        str[start] = ec;
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+char** generateDefaultStringArray()
+{
+    int ns = 4;
+    char* strings[] = { "Edward", "Test", "End", "Start"} ;
+    char** arr = new char*[ns + 1];     // +1 for nullptr to mark the end of arr
+
+    for(int i = 0; i < ns; i++)
+    {
+        int numChars = stringLen(strings[i]);
+        char* newString = new char[numChars+1];
+        for(int j = 0; j < numChars; j++)
+        {
+            char c = strings[i][j];
+            newString[j] = c;
+        }
+        newString[numChars] = '\0';
+        arr[i] = newString;
+    }
+
+    arr[ns] = nullptr;
+    return arr;
+}
+
+template <typename T>
+void printArr(T** arr)
+{
+    for(int i = 0; arr[i] != nullptr; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
+}
+
 int main(int argc, char** argsv)
 {
-    const char* arr[] = {"Edward", "Test", "End", "Start"};
+    char** arr = generateDefaultStringArray();
 
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    for(int i = 0; arr[i] != nullptr; i++)
     {
-        size_t len = stringLen(arr[i]) + 1;
-        std::cout << "Length of " << arr[i] << ": " << len << std::endl;
-        char* destination = new char[len];
-
-        stringCopy(destination, arr[i]);
-        std::cout << "Copy: " << destination << std::endl;
-        delete[] destination;
+        reverseStr(arr[i]);
     }
+
+    printArr(arr);
+    
+    delete[] arr;
 }
